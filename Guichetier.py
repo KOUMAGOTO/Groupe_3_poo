f1279d26259e2441199612f127dff2c9d52f7a82
Bemadji Arsene <57248473+Bemadji@users.noreply.github.com>
2019-11-09 23:20:42 +0100

    Update Guichetier.py

--- Guichetier.py
+++ Guichetier.py
@@ -2,5 +2,22 @@ Gestionnaire.py
 # Bemadji Arséne
 #18A874FS
 class Guichetier(PersonneBank):
-  def_init_(self):
- 
+    def __init__ (self, nom, solde=0, montant=0):
+        self.montant = montant
+        self.solde = 2000
+    def IdeeSolde(self) :    
+        print("votre solde est:\n", self.solde)
+    def versement(self):
+       montant=input("Saisir le montant à verser\n")
+       montant=int(montant)
+       self.solde += montant
+       print("votre nouveau solde est:\n", self.solde)
+    def retrait(self):
+       somme = input("entrer le montant a retirer\n")
+       somme = int(somme)
+       if somme > self.solde:
+          print("solde insuffisant")
+       else:
+          self.solde -= somme
+          print("retrait de {} effectuer avec succes\n".format(somme))
+          print("nouveau solde est:\n", self.solde) 
