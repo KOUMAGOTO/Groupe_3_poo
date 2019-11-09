7b322d2553f52bdf42c5355719edc9d64adf208c
OLYMPE20 <57182463+OLYMPE20@users.noreply.github.com>
2019-11-09 20:49:26 +0100

    Update Gestionnaire.py

--- Gestionnaire.py
+++ Gestionnaire.py
@@ -1,1 +1,16 @@
-
+# Mbaiondoum Ngonmiadje
+# 17A513FS l2info
+Class Gestionnaire:
+  def __init__(self):
+     self.user = ""
+     self.code1 = ""
+     self.code2 = ""
+  def ajout(self):
+     self.user =  input("Entrez votre nom svp\n")
+     self.code1 = input("Entrez votre code\n")
+     self.code2 = input("Confirmez votre code\n")
+     while self.code1 != self.code2:
+          print("Code incorrect, veuillez reesayer\n")
+          self.code1 = input("Entrez votre code\n")
+          self.code2 = input("Confirmez votre code\n")
+     print("Votre compte est créé avec succès \n") 
