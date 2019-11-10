78bf9b8e39bd23716aa92492ec31059d7811fbda
Patrick Koumagoto Mboitam <56525944+KOUMAGOTO@users.noreply.github.com>
2019-11-10 23:01:34 +0100

    Update PersonneBank.py
    La classe mere est écrite par KOUMAGOTO MBOITAM PATRICK 18A180FS

--- PersonneBank.py
+++ PersonneBank.py
@@ -6,9 +6,6 @@ class PersonneBank:
         self.nom = nom
         self.prenom = prenom
 
-    def __str__(self):
-        return "nom :" + self._nom + "\prenom:" +self._prenom
-
     def _get_nom(self):
         return self._nom
     def _set_nom(self,n):
