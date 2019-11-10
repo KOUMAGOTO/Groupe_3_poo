6e93b07cdc29f66b91b420ef5c5b8630f395076f
Patrick Koumagoto Mboitam <56525944+KOUMAGOTO@users.noreply.github.com>
2019-11-10 12:50:54 +0100

    Create PersonneBank.py

---
+++ PersonneBank.py
@@ -0,0 +1,26 @@
+#la super classe est écrite par KOUMAGOTO Mboîtam Patrick
+#Matricule:18A180Fs
+
+class PersonneBank:
+    def __init__(self,nom = "",prenom = ""):
+        self.nom = nom
+        self.prenom = prenom
+
+    def __str__(self):
+        return "nom :" + self._nom + "\prenom:" +self._prenom
+
+    def _get_nom(self):
+        return self._nom
+    def _set_nom(self,n):
+        self._nom = n
+    nom = property(_get_nom,_set_nom)
+
+    def _get_prenom(self):
+        return self._prenom
+    def _set_prenom(self,n):
+        self._prenom = p
+    nom = property(_get_prenom,_set_prenom)
+    def afficher(self):
+        self.nom = input(“veillez entrer votre nom\n”)
+        self.prenom=input(“veillez entrer votre prenom\n”)
+        print(“votre nom, prénom est = “)
