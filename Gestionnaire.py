3d1297eac0ca5e0a2cd66fb7f7369d4ffba0d76b
ALLARAMADJI MBAINDI GHISLAIN <57015986+mbaindighislain@users.noreply.github.com>
2019-11-10 12:17:41 +0100

    Update Gestionnaire.py

--- Gestionnaire.py
+++ Gestionnaire.py
@@ -21,37 +21,39 @@ Class Gestionnaire:
      print("veillez vous identifier pour modifier votre compte\n")
       iden = input("veuillez entrer votre id\n")
       cod = input(" veuillez entrer votre code\n")
-      if (iden == self.id and cod == self.password1):
+      if (iden == self.user and cod == self.code1):
      print("Que vouliez vous modifier\n1- mon id\n2- mon mot de passe\n")
        n = input("\n")
        n = int(n)
        if n == 1:
-     self.id = input("veuillez entrer votre nouveau id\n")
+     self.user = input("veuillez entrer votre nouveau id\n")
      print("id modifier avec sucess votre nouveau id est\n", self.id)
      elif n == 2:
-     self.password1 = input("veuillez entrer votre nouveau mot de passe\n")
-     self.password2 = input("veuillez confirmer votre nouveau mot de passe\n")
-      while self.password1 != self.password2:
+     self.code1 = input("veuillez entrer votre nouveau mot de passe\n")
+     self.code2 = input("veuillez confirmer votre nouveau mot de passe\n")
+      while self.code1 != self.code2:
         print("les nouveuax mots de passe sont different veuiller reessayer\n")
-         self.password1 = input("entrer votre nouveau mot de passe\n")
-         self.password2 = input("confirmer votre nouveau mot de passe\n")
+         self.code1 = input("entrer votre nouveau mot de passe\n")
+         self.code2 = input("confirmer votre nouveau mot de passe\n")
          print("votre mot de passe a ete modifier avec succes\nvotre nouveau mot de passe est:\n", self.password1)
          else:
              print("option non disponible") 
           else: 
             print("desole ce compte n'existe pas\n")  
 def suprim(self): #defini la fonction supprimer
-peint(veuillez vous identifier pour supprimer votre compte\n")
-ide = input("entrer votre id\n")
-code = input("entrez votre mot de passe\")
-if ide != self.id:
-Peint("compte n'existe pas \n")
-elif code != self.password1:
-Print(" compte n'existe pas \n")
-else:
-sel.id = ""
-self.password1 = ""
-print("votre compte a ete supprime)
+    print(veuillez vous identifier pour supprimer votre compte\n")
+    ide = input("entrer votre id\n")
+    code = input("entrez votre mot de passe\")
+    if ide != self.id:
+       Print("compte n'existe pas \n")
+    elif code != self.code1:
+       Print(" compte n'existe pas \n")
+    else:
+       sel.user = ""
+       self.password1 = ""
+       print("votre compte a ete supprime)
+
+
 ab = Gestion ()
 ab.ajout()
 ab.modif()
