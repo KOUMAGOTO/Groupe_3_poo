f66fd535415f134fcdc000a2f1c05914fad922ce
Kodjiyanouba Christiane <57589104+kodjichristiane@users.noreply.github.com>
2019-11-10 22:56:57 +0100

    Update Guichetier.py

--- Guichetier.py
+++ Guichetier.py
@@ -20,3 +20,18 @@ class Guichetier(PersonneBank):
           print("vous avez effectuer un retrait de ", som)
       else:
           print("solde insuffisant pour faire le retrait")
+def retrait(self):
+    som=input("entrer le montant à retirer\n")
+    som=int(som)
+    if som<=self.solde:
+        print("vous avez effectué un retrait de", som)
+    else:
+        print("solde insuffisant pour faire le retrait")
+def virement(self, montant):
+   self.retrait(self)
+   montant.versement(self)
+   print("le virement a ete effectuer")
+abo = "Guichetier"()
+abo.retrait()
+abo.versement(5500)
+abo.virement(2000)
