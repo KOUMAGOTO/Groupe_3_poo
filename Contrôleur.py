04819e5df2a04683d9315aac40e82f4bcbe9f282
ARISTIDE15 <57256650+ARISTIDE15@users.noreply.github.com>
2019-11-13 17:44:03 +0100

    Update Contrôleur.py

--- Contrôleur.py
+++ Contrôleur.py
@@ -1,1 +1,11 @@
-# Groupe_3_poo
+#cette sous-classe est écrite par DOUMNINGA KLAMTE ARISTIDE 18B598Fs
+class controleur (PersonneBank):
+    solde=int(input("solde"))
+    montant=int(input("montant"))
+    if solde<=0:
+        print("pas de transaction")
+    else:
+        if solde<montant :
+            print ("pas de transaction")
+        else:
+            print ("transaction=ok")
