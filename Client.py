372e093e679ef235643165b33d535e8d7fa896d8
issa rouzoumka yadia <57112368+Issa66@users.noreply.github.com>
2019-11-13 22:29:54 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,29 +1,27 @@
 Issa rouzoumka yadia 18A919FS
-Classe Client: 
-def init (self, numero): 
-self.numero = numero 
-self.solde = 10000 
-def InfoSolde (self): 
-impression ("votre solde est: \ n", self.solde) 
-def renvoi ( self): 
-som = input ("combien vouliez vous verser? \ n") 
-som = int (som) 
-self.solde + = som 
-print ("votre nouveau solde: \ n", self.solde) 
-def retrait (self) : 
-somme = input ("entrer le montant à retirer \ n") 
-somme = int (somme) 
-si somme> self.solde: 
-print ("solde insuffisant") 
-else: 
-self.solde - = somme 
-print ("retrait de {} effectuer avec succes \ n ".format (somme))
-print ("nouveau solde est: \ n", self.solde) 
-issa = Client (12) 
-issa.InfoSolde () 
-issa.versement () 
-issa.retrait () 
-issa.retrait () 
-issa.retrait () 
-issa.versement () )
 
+classe Client:
+   def __init __ (self, numero):
+      self.numero = numero
+      self.solde = 10000
+   def InfoSolde(self):
+       print("votre solde est:\n", self.solde)
+   def versement(self):
+       som = input("combien vouliez vous verser ?\n")
+       som = int(som)
+       self.solde += som
+       print("votre nouveau solde est:\n", self.solde)
+   def retrait(self):
+       somme = input("entrer le montant a retirer\n")
+       somme = int(somme)
+       if somme > self.solde:
+          print("solde insuffisant")
+       else:
+          self.solde - = somme
+          print ("retrait de {} effectuer avec succes \ n" .form (somme))
+          print("nouveau solde est:\n", self.solde)
+Issa = Client(12)
+Issa.InfoSolde()
+Issa.versement()
+Issa.retrait()
+Issa.versement ():
