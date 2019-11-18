b3a69314cfa1381fdd5ba632a0988ea33b1c9ad3
Togyanouba Olivier <57250815+42olver@users.noreply.github.com>
2019-11-18 23:53:36 +0100

    Update Compte.py

--- Compte.py
+++ Compte.py
@@ -1,1 +1,11 @@
-
+class comptebancaire:
+   def __init__( self,nom='dupond',solde=1000):
+      self.nom=nom
+      self.solf=solde
+   def depot (self, solde):
+      self.solde=somme
+   def retrait(self, somme):
+       self.solde=somme
+   def affiche( self):
+      print (" le solde du compte bancaire de %s est  %.2f euros( self.nom, self.solde)")
+ 
